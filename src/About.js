585a33d960e313711b279c1f8fba817c0cba517f
ReeceRenninger <renningerreece@gmail.com>
2023-03-27 16:46:33 -0700

    added small about us descriptions and styled to flex

--- src/About.js
+++ src/About.js
@@ -1,10 +1,33 @@
 import { Component } from "react";
+import Card from 'react-bootstrap/Card';
 
 class Profile extends Component {
 
   render() {
-    /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <section id="cards">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Nicholas Brown</Card.Title>
+            <Card.Text>
+              Nicholas is the CSS wizard while being a full time dad/farmer/dog breeder/husband.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Reece Renninger</Card.Title>
+            <Card.Text>
+              Reece is going to be probably become a backend dev because he has no styling capabilities.
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+      </section>
+    )
   }
 };
 
